57e72211c8c4000a26987015823e78a393633e94
tyler-Github <51767087+tyler-Github@users.noreply.github.com>
2022-04-20 01:48:29 +0100

    Delete randomAd.js

--- randomAd.js
+++
@@ -1,7 +0,0 @@
-window.onload = choosePic;
-
-var myPix = new Array("");
-
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("myPicture").src = myPix[randomNum];
